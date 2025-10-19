e5680cf3cc2840588dbe047a2db64904c7800f2e
Robert Schmidt <93723905+Mindstormer314@users.noreply.github.com>
2025-10-19 15:48:43 -0600

    MUSIC (Partly)

--- Scenes/Menus/main_menu.gd
+++ Scenes/Menus/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@export var fade_duration : float
+@export var fade_intensity : float
+
+var audio_tween : Tween
+
 #Called when the node enters the scene tree for the first time.
 func _ready(): #puts focus on top button
 	$VBoxContainer/Start.grab_focus()
@@ -9,7 +14,16 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
+	audio_tween = create_tween()
+	audio_tween.tween_method(audio_fade, 0.0, 1.0, fade_duration)
+	audio_tween.play()
+	await audio_tween.finished
+	
 	get_tree().change_scene_to_file("res://Scenes/L1.tscn")
+
+func audio_fade(ratio : float):
+	%MainThemePlayer.volume_db = -fade_intensity*ratio
+	
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menus/credits.tscn")
