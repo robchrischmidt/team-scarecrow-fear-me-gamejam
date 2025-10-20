dc1b604a75381a1408d94aad7b72b8c467498be8
Adara Noble <nobleadara1@gmail.com>
2025-10-19 23:33:48 -0600

    Added new chandelier sprites and slight logic change

--- Objects/Chandelier/Chandelier.gd
+++ Objects/Chandelier/Chandelier.gd
@@ -2,13 +2,11 @@ extends StaticBody2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if(body.get_instance_id() == $"../Player".get_instance_id()):
-		$AnimatedSprite2D.play("stable")
+	$AnimatedSprite2D.play("stable")
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if(body.get_instance_id() == $"../Player".get_instance_id()):
-		$AnimatedSprite2D.play("sway")
+	$AnimatedSprite2D.play("sway")
 
 func _process(delta : float) -> void:
 	var player : CharacterBody2D = get_tree().get_first_node_in_group("player")
